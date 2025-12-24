36edf60888b9903c9645fb32417f2dbd13769821
isle27 <betelhemwubie28@gmail.com>
2025-12-24 03:52:01 -0800

    done nn

--- oprations/config.py
+++ oprations/config.py
@@ -1,1 +1,8 @@
  # Load env variables, global settings
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL")
--- oprations/utils.py
+++ oprations/utils.py
@@ -1,1 +1,20 @@
 # Helper functions (random choice, delays, logging)
+import requests
+from config import BASE_URL
+
+
+def login(email,password):
+    url = f"{BASE_URL}/update-user/login"
+    payload = {
+    "email": email,
+    "password": password
+    }
+    response = requests.post(url, json=payload)
+    if response.ok:
+        print("Login successful")
+        return response.json()  # usually contains token
+    else:
+        print("Login failed:", response.status_code, response.text)
+        return None
+    
+
